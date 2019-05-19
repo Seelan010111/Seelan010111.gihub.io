42044ae77cdaa3a7dc46e7ac9c8e855ebf103244
Seelan <norwell.sigwebela@prodigyfinance.com>
2019-05-20 01:28:48 +0200

    Updates

---
+++ precache-manifest.c3f9a013f0b91a40d70187265fa450f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77f13970c37d62dcd5c20aa6117c227",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04a05afa8dca0e325ea3",
+    "url": "/static/css/main.f8e146a7.chunk.css"
+  },
+  {
+    "revision": "7a88d75c73256b63a316",
+    "url": "/static/js/2.3eaf4e89.chunk.js"
+  },
+  {
+    "revision": "04a05afa8dca0e325ea3",
+    "url": "/static/js/main.586e230f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
